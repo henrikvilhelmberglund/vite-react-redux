f1955d3e6557b352f41834a71eb24c818d5a70c1
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-13 09:04:40 +0200

    feat: :sparkles: add redux stuff

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
 import "virtual:uno.css";
-import { Provider } from "react-redux";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "generouted/react-router";
+import { Provider } from "react-redux";
+
+import store from "./redux/configureStore";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <RouterProvider router={createBrowserRouter(routes)} />
     </Provider>
   </React.StrictMode>
