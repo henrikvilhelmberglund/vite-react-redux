ff64d798e68a22f4216a33e2b411ebb13c5e3b44
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-13 09:50:33 +0200

    fix: :art: make it clearer that it is store

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 export default function Counter() {
   const dispatch = useDispatch();
-  const count = useSelector((state) => state.counter.count);
+  const count = useSelector((store) => store.counter.count);
   return (
     <div>
       <h2>{count}</h2>
